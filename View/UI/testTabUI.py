753576c1f0c45577d4833326d4473bc4f071cf46
Anna Shimizu <ashi725@aucklanduni.ac.nz>
2023-04-19 00:34:04 +1200

    Created rough GUI for test tab

--- View/UI/testTabUI.py
+++ View/UI/testTabUI.py
@@ -1,17 +1,58 @@
-from pathlib import Path
-import sys
-import cv2
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QWidget, QTabWidget, QVBoxLayout, QLabel, QComboBox, QPushButton, QInputDialog
-from PyQt5.QtGui import QPixmap
-
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
 
 class TestTab(QWidget):
     def __init__(self):
         super().__init__()
 
-        label = QLabel("test!")
-
         vbox = QVBoxLayout()
-        vbox.addWidget(label)
+        vbox.setAlignment(Qt.AlignTop | Qt.AlignCenter)
+
+        vboxHyperparameters = QVBoxLayout()
+
+        cnnLabel = QLabel("CNN Name: ")
+        vboxHyperparameters.addWidget(cnnLabel)
+
+        batchsizeLabel = QLabel("Batch Size: ")
+        vboxHyperparameters.addWidget(batchsizeLabel)
+
+        epochNumLabel = QLabel("Epoch Number: ")
+        vboxHyperparameters.addWidget(epochNumLabel)
+
+        loadModel = QPushButton("Load model from file")
+        loadModel.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        loadModel.clicked.connect(self.show_file_dialog)
+
+        hbox = QHBoxLayout()
+        hbox.addLayout(vboxHyperparameters)
+        hbox.addWidget(QLabel("                                     "))
+        hbox.addWidget(loadModel)
+
+        vbox.addLayout(hbox)
+
+        testUsingLabel = QLabel("Test using: ")
+        testUsingLabel.setStyleSheet("font-size: 16px; padding-top: 30px")
+        hboxLabel = QHBoxLayout()
+        hboxLabel.addWidget(testUsingLabel)
+        hboxLabel.setAlignment(Qt.AlignCenter)
+        vbox.addLayout(hboxLabel)
+
+        hboxButtons = QHBoxLayout()
+        datasetImagesButton = QPushButton("Dataset images")
+        datasetImagesButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        datasetImagesButton.clicked.connect(self.show_dataset_dialog)
+
+        webcamButton = QPushButton("Webcam images")
+        webcamButton.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        hboxButtons.addWidget(datasetImagesButton)
+        hboxButtons.addWidget(webcamButton)
+
+        vbox.addLayout(hboxButtons)
+
         self.setLayout(vbox)
+
+    def show_file_dialog(self):
+        fname = QFileDialog.getOpenFileName(self, 'Open file', './')
+
+    def show_dataset_dialog(self):
+        print("dataset")
